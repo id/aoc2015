faae865d235600b41b21994f50ba749fcf780446
Ivan Dyachkov <dev@dyachkov.org>
2021-10-22 19:41:24 +0200

    day 04

---
+++ aoc/day_04.py
@@ -0,0 +1,27 @@
+import hashlib
+
+def part1(data):
+    i = 0
+    while True:
+        hash = hashlib.md5(f"{data}{i}".encode("utf-8")).hexdigest()
+        if (hash.startswith('00000')):
+            break
+        i += 1
+    return i
+
+def part2(data):
+    i = 0
+    while True:
+        hash = hashlib.md5(f"{data}{i}".encode("utf-8")).hexdigest()
+        if (hash.startswith('000000')):
+            break
+        i += 1
+    return i
+
+def main():
+    with open('data/04.txt') as f:
+        data = f.read().rstrip('\n')
+    print(part1(data))
+    print(part2(data))
+
+main()
---
+++ tests/04_test.py
@@ -0,0 +1,9 @@
+import pytest
+from aoc import day_04
+
+def test_part1():
+    assert 609043 == day_04.part1('abcdef')
+    assert 1048970 == day_04.part1('pqrstuv')
+
+def test_part2():
+    assert 0 == 0
